e3278b6f5fb1bf5a86f54dce12d5c3a13a1b96d3
matheusfonsecagoncalves-blip <matheusfonsecagoncalves@gmail.com>
2026-08-19 16:59:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+algoritmo "Levantamento_PCD"
+
+var
+   total, pcd: inteiro
+   percentual: real
+
+inicio
+
+   total <- 20
+   pcd <- 6
+
+   percentual <- (pcd * 100) / total
+
+   escreval("Total de alunos: ", total)
+   escreval("Quantidade de alunos PCD: ", pcd)
+   escreval("Percentual de PCD: ", percentual:0:2, "%")
+
+fimalgoritmo
+Saída
+Total de alunos: 20
+Quantidade de alunos PCD: 6
+Percentual de PCD: 30,00%
+Cálculo
+percentual = (6 × 100) / 10
+percentual = 30%
+
+Resultado: a escola possui 20 alunos, sendo 6 alunos PCD, o que corresponde a 30% do total.
